ca0bee3361dd81573efc77fb699269f0d2d152d3
DegenDev3 <Devin.Dillon@MyKenzie.snhu.edu>
2023-06-11 17:34:23 -0400

    added ToolRecord TAKE3

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/ToolRecord.java
@@ -0,0 +1,96 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@DynamoDBTable(tableName = "ToolDatabase")
+public class ToolRecord {
+    private int toolId;
+    private String owner; //GSI //username from UserDB
+    private String toolName;
+    private boolean isAvailable;
+    private String description;
+    private List<String> comments;
+    private String borrower;
+
+    public ToolRecord() {
+    }
+
+    public ToolRecord(int toolId, String owner, String toolName, boolean isAvailable, String description, String borrower) {
+        this.toolId = toolId;
+        this.owner = owner;
+        this.toolName = toolName;
+        this.isAvailable = isAvailable;
+        this.description = description;
+        this.borrower = borrower;
+    }
+
+    @Id
+    @DynamoDBHashKey
+    public int getToolId() {
+        return toolId;
+    }
+
+    public void setToolId(int toolId) {
+        this.toolId = toolId;
+    }
+
+    @DynamoDBAttribute
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @DynamoDBAttribute
+    public String getToolName() {
+        return toolName;
+    }
+
+    public void setToolName(String toolName) {
+        this.toolName = toolName;
+    }
+
+    @DynamoDBAttribute
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @DynamoDBAttribute
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @DynamoDBAttribute
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+
+    @DynamoDBAttribute
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+}
+
