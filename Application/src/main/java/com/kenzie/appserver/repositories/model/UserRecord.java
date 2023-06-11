1771faccaa89009d934da028425de5d7568ef5df
DominicJ98 <dominic.jimenez@mykenzie.snhu.edu>
2023-06-11 14:11:56 -0700

    Added models for User controller, repository, and Service

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
@@ -0,0 +1,49 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import java.util.Objects;
+
+import com.amazonaws.services.dynamodbv2.xspec.S;
+import org.springframework.data.annotation.Id;
+
+@DynamoDBTable(
+        tableName = "UserDatabase"
+)
+public class UserRecord {
+    private String name;
+    private String userName;
+    private String password;
+
+    public UserRecord(String name, String userName, String password){
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Id
+    @DynamoDBHashKey(
+            attributeName = "name")
+    public String getName(){return this.name;}
+
+    public void setName(String userId){this.name = name;}
+
+    @DynamoDBAttribute(attributeName = "userName")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @DynamoDBAttribute(attributeName = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Application/src/main/java/com/kenzie/appserver/service/model/User.java
@@ -0,0 +1,46 @@
+package com.kenzie.appserver.service.model;
+
+public class User {
+    private String name;
+    private String userName;
+    private String password;
+    private Integer zipCode; // to be implemented later
+
+    public User(String name, String userName, String password){
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+}
