7ef0422e48bac9f14cc337fb2efbd00f52209048
Aaron Tukuafu <aaron.tukuafu@mykenzie.snhu.edu>
2023-07-09 14:27:06 -0700

    UserControllerTest unit tests completed

--- Application/src/test/java/com/kenzie/appserver/service/UserServiceTest.java
+++ Application/src/test/java/com/kenzie/appserver/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.kenzie.appserver.service;
 
+import com.kenzie.appserver.Cache.CachingUserDao;
 import com.kenzie.appserver.controller.model.UserCreateRequest;
 import com.kenzie.appserver.controller.model.UserResponse;
 import com.kenzie.appserver.repositories.UserRecordRepository;
@@ -8,28 +9,32 @@ import com.kenzie.appserver.service.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class UserServiceTest {
 
-    private UserService userService;
+    @Mock
+    private CachingUserDao cache;
 
+    @Mock
     private UserRecordRepository userRecordRepository;
-
+    @InjectMocks
+    private UserService userService;
 
     @BeforeEach
     public void setUp() {
-        userRecordRepository = Mockito.mock(UserRecordRepository.class);
-        userService = new UserService(userRecordRepository);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
@@ -41,48 +46,51 @@ public class UserServiceTest {
         userRecord.setUsername(username);
         userRecord.setPassword(password);
 
-        when(userRecordRepository.existsById(username)).thenReturn(true);
+        when(cache.findById(username)).thenReturn(Optional.empty());
         when(userRecordRepository.findById(username)).thenReturn(Optional.of(userRecord));
 
         //WHEN
         boolean findingUser = userService.authenticator(username, password);
 
         //THEN
-        Assertions.assertTrue(findingUser, "User Found!");
+        assertTrue(findingUser, "User Found!");
+        verify(cache).addToCache(userRecord);
     }
 
     @Test
-    public void invalidUsername_CannotFindUser(){
+    public void invalidCredentials_CannotFindUser(){
         //GIVEN
         String username = "testuser";
         String password = "testpassword";
+        UserRecord userRecord = new UserRecord("John Doe", username, "wrong password");
 
+        when(cache.findById(username)).thenReturn(Optional.of(userRecord));
         when(userRecordRepository.existsById(username)).thenReturn(false);
 
         //WHEN
         boolean findingUser = userService.authenticator(username, password);
 
         //THEN
-        Assertions.assertFalse(findingUser, "Username is Invalid!");
+        assertFalse(findingUser, "User Not Found!");
     }
 
     @Test
-    public void invalidPassword_CannotFindUser(){
+    public void testAuthenticator_UserNotInCache(){
         //GIVEN
         String username = "testuser";
         String password = "testpassword";
-        UserRecord userRecord = new UserRecord();
-        userRecord.setUsername(username);
-        userRecord.setPassword("wrongpassword");
+        UserRecord userRecord = new UserRecord("testuser", username, password);
 
-        when(userRecordRepository.existsById(username)).thenReturn(true);
+        when(cache.findById(username)).thenReturn(Optional.empty());
         when(userRecordRepository.findById(username)).thenReturn(Optional.of(userRecord));
 
         //WHEN
         boolean findUser = userService.authenticator(username,password);
 
         //THEN
-        Assertions.assertFalse(findUser, "The Password is invalid");
+        assertTrue(findUser);
+        verify(userRecordRepository, times(1)).findById(username);
+        verify(cache, times(1)).addToCache(userRecord);
     }
 
     @Test
@@ -114,25 +122,71 @@ public class UserServiceTest {
         UserResponse userResponse = userService.createNewUser(userCreateRequest);
 
         // THEN
-        Assertions.assertNotNull(userResponse, "The UserResponse should not be null");
+        assertNotNull(userResponse, "The UserResponse should not be null");
         Assertions.assertEquals(userCreateRequest.getName(), userResponse.getName(), "The name should match");
         Assertions.assertEquals(userCreateRequest.getUsername(), userResponse.getUsername(), "The username should match");
         Assertions.assertEquals(userCreateRequest.getPassword(), userResponse.getPassword(), "The password should match");
     }
 
-//    @Test
-//    public void canCreateNewUser(){
-//        User user = new User("John Doe", "johndoe", "password123");
-//        user.setName("John Doe");
-//        user.setUsername("johndoe");
-//        user.setPassword("password123");
-//
-//        UserRecord savedRecord = new UserRecord();
-//        when(userRecordRepository.save(any(UserRecord.class))).thenReturn(savedRecord);
-//
-//        User result = userService.createNewUser(user);
-//
-//        verify(userRecordRepository).save(any(UserRecord.class));
-//        Assertions.assertEquals(user, result);
-//    }
+    @Test
+    public void userNotFound_ReturnFalse(){
+        //GIVEN
+        String username = "John";
+        String password = "password";
+
+        when(cache.findById(username)).thenReturn(Optional.empty());
+        when(userRecordRepository.findById(username)).thenReturn(Optional.empty());
+
+        //WHEN
+        boolean result = userService.authenticator(username, password);
+
+        //THEN
+        assertFalse(result);
+    }
+
+    @Test
+    public void getUser_UserExistsInCache(){
+        //GIVEN
+        String username = "fakeuser";
+        UserRecord userRecord = new UserRecord("fakeuser", username, "password");
+
+        when(cache.findById(username)).thenReturn(Optional.of(userRecord));
+
+        //WHEN
+        UserResponse userResponse = userService.getUser(username);
+
+        //THEN
+        Assertions.assertNotNull(userResponse);
+        Assertions.assertEquals(userRecord.getName(), userResponse.getName());
+        Assertions.assertEquals(userRecord.getUsername(), userResponse.getUsername());
+        Assertions.assertEquals(userRecord.getPassword(), userResponse.getPassword());
+    }
+
+    @Test
+    public void getUser_UserNotInCache(){
+        //GIVEN
+        String username = "John";
+
+        when(cache.findById(username)).thenReturn(Optional.empty());
+
+        //WHEN
+        UserResponse userResponse = userService.getUser(username);
+
+        //THEN
+        assertNull(userResponse);
+    }
+
+    @Test
+    public void deleteUser_ReturnsTrue(){
+        //GIVEN
+        String username = "fakeuser";
+
+        //WHEN
+        userService.deleteUser(username);
+
+        //THEN
+        verify(userRecordRepository,times(1)).deleteById(username);
+        verify(cache, times(1)).invalidate(username);
+    }
+
 }
